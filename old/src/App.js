75e50d33ceea3880a1a436822761df30f337b773
Daisuke Yamato <yamato.hep.fermicern@gmail.com>
2024-11-05 22:03:07 +0900

    module 4

---
+++ old/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+
+// import Header from "./components/Header";
+import Header from "./Header";
+import Nav from "./Nav";
+import Main from "./Main";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <nav>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </nav>
+  );
+}
+
+export default App;
